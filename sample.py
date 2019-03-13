fdc8a70f6ed93f19c46380ee2bd55ec4e274b422
avyyaaggarwal <avyya.aggarwal@aricent.com>
2019-03-13 11:36:40 +0530

    first commit

---
+++ sample.py
@@ -0,0 +1,16 @@
+def sum():
+  a = 10
+  b = 20
+  c = a + b
+  print(c)
+  
+def subtract():
+  a = 10
+  b = 20
+  c = b - a
+  print(c)
+  
+  
+  
+if __name__ == '__main__':
+  sum()
---
+++ test.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+def mutliply():
+  a = 10
+  b = 20
+  c = a * b
+  print(c)
+  
+def divide():
+  a = 10
+  b = 20
+  c = b/a
+  print(c)
+  
+  
+  
+if __name__ == '__main__':
+  sum()
+
